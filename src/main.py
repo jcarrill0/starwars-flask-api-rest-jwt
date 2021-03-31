0fd1858fe1f386aa648372d14dc85f5c28754d55
jcarrill0 <josecarrillo8@gmail.com>
2021-03-31 19:05:57 +0000

    Proyecto actualizado

--- src/main.py
+++ src/main.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planet, Favorite
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -31,12 +30,33 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def list_user():
+    all_user = User.query.all()
+    all_user = list(map(lambda x: x.serialize(), all_user))
+    return jsonify(all_user), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    body = request.get_json()
+
+    if body is None:
+        return "The request body is null", 400
+    if 'email' not in body:
+        return 'You need to specify the email', 400
+    if 'password' not in body:
+        return 'You need to specify the password', 400
+    
+    user = User()
+    user.email = body['email']
+    user.password = body['password']
+    user.is_active = True
+
+    db.session.add(user)
+    db.session.commit()
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "PURA VIDA"
     }
-
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
--- src/models.py
+++ src/models.py
@@ -4,9 +4,10 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+    favorites = db.relationship('Favorite', backref="user", uselist=False, lazy="select")
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +15,73 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username" : self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "password" : self.password,
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True nullable=False)
+    birth_year = db.Column(db.String(100), nullable=False)
+    gender = db.Column(db.String(100), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    skin_color = db.Column(db.String(100), nullable=False)
+    eye_color = Column(db.String(100), nullable=False)
+    favorites = relationship('favorite')
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name" : self. name,
+            "birth_year" : self.birth_year,
+            "gender" : self.gender,
+            "height" : self.height,
+            "skin_color" : self.skin_color,
+            "eye_color" : self.eye_color
+                    
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    climate = db.Column(db.String(100), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer), nullable=False
+    rotation_period = db.Column(db.Integer, nullable=False)
+    diamater = Column(db.Integer, nullable=False)
+    favorites = relationship('favorite')
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name" : self.name,
+            "climate" : self.climate,
+            "population" : self.population,
+            "orbital_period" : self.orbital_period,
+            "rotation_period" : self.rotation_period,
+            "diamater" : self.diamater
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, ForeignKey('people.id'))
+    planet_id = db.Column(db.Integer, ForeignKey('planet.id'))
+
+    # def __repr__(self):
+    #     return '<Favorite %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id":self.people_id,
+            "planet_id":self.planet_id
         }
