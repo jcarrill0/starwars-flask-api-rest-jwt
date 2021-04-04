b612af9371167f7b77d74a2ba03dac8cc0e43588
jcarrill0 <josecarrillo8@gmail.com>
2021-04-04 22:55:03 +0000

    Model finish

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
-db.init_app(app)
+db.init_app(app) ## inicializo mi base de datos con mi app (db = SQLAlchemy(app))
 CORS(app)
 setup_admin(app)
 
--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String(500), nullable=False)
+    password = db.Column(db.Text, nullable=False)
     favorites = db.relationship('Favorite', backref="user", uselist=False, lazy="select")
 
     def __repr__(self):
@@ -22,13 +22,13 @@ class User(db.Model):
 
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True nullable=False)
-    birth_year = db.Column(db.String(100), nullable=False)
-    gender = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    birth_year = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.String(50), nullable=False)
     height = db.Column(db.Integer, nullable=False)
-    skin_color = db.Column(db.String(100), nullable=False)
-    eye_color = Column(db.String(100), nullable=False)
-    favorites = relationship('favorite')
+    skin_color = db.Column(db.String(50), nullable=False)
+    eye_color = db.Column(db.String(50), nullable=False)
+    favorites = db.relationship('Favorite', backref='people', lazy=True)
 
     def __repr__(self):
         return '<People %r>' % self.name
@@ -47,13 +47,13 @@ class People(db.Model):
 
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    climate = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    climate = db.Column(db.String(50), nullable=False)
     population = db.Column(db.Integer, nullable=False)
-    orbital_period = db.Column(db.Integer), nullable=False
+    orbital_period = db.Column(db.Integer, nullable=False)
     rotation_period = db.Column(db.Integer, nullable=False)
-    diamater = Column(db.Integer, nullable=False)
-    favorites = relationship('favorite')
+    diamater = db.Column(db.Integer, nullable=False)
+    favorites = db.relationship('Favorite', backref='planet', lazy=True)
 
     def __repr__(self):
         return '<Planet %r>' % self.name
@@ -71,9 +71,9 @@ class Planet(db.Model):
 
 class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey('user.id'))
-    people_id = db.Column(db.Integer, ForeignKey('people.id'))
-    planet_id = db.Column(db.Integer, ForeignKey('planet.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
 
     # def __repr__(self):
     #     return '<Favorite %r>' % self.username
